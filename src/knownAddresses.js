d1764d3c693ab3c0eaca3b7875daab0e81574ef6
lowzyyy <peratheking@aol.com>
2024-01-22 18:28:08 +0100

    added more addresses

--- src/knownAddresses.js
+++ src/knownAddresses.js
@@ -27,4 +27,144 @@ export const knownAddresses = new Map([
     "19EeatT7mdZAK36SFRj4zf6Wv98fESGv8CN42YXnQTcFQ",
     { type: "Exchange", name: "Coinmetro" },
   ],
+  [
+    "1HSajUFSnCbBV5cXtwsCkWQ8jVAwBTjajun9qMXW8kG6m",
+    { type: "Pool", name: "Metapool" },
+  ],
+  [
+    "18BHr6VE31bhSHJoBdmkrDGPKj1H3QQ6Ui2kakYbAWBqi",
+    { type: "Pool", name: "Metapool" },
+  ],
+  [
+    "13vzh4LZJrTekg53tcAYzz9ETNNz12BaUHsGmjqPpXUTE",
+    { type: "Pool", name: "Metapool" },
+  ],
+  [
+    "159STVKgoVi2KLwJSY7LDewjbq9b5QDw259QVS3sBxqvt",
+    { type: "Pool", name: "Metapool" },
+  ],
+  [
+    "13vPxxvdLG5Z9pmzbeb7xLbxpqX8HN7hT3WbyE36eqMeV",
+    { type: "Pool", name: "Metapool" },
+  ],
+  [
+    "1AYR6H9LqCUN6BsRCQg2eiHHUYZ6hBbvyRz2AYzda1YVa",
+    { type: "Pool", name: "Metapool" },
+  ],
+  [
+    "163CahfVB61xn9pY8dvFLznETGzCopPTwFTvHqmno3Fq6",
+    { type: "Pool", name: "Metapool" },
+  ],
+  [
+    "1Ds3kYiTNkKMkPSs6JothpZAALV3dpevwSs16xUApf3V3",
+    { type: "Pool", name: "Woolypooly" },
+  ],
+  [
+    "165g1Vkx8gnDZ3CCxPE81fgYeNaH8HnBvYQDBXr9U4AT7",
+    { type: "Pool", name: "Woolypooly" },
+  ],
+  [
+    "19nnsAodfSnrnVmmPrHN3ZaycHFz4NSgMmmW9YG3TEdGx",
+    { type: "Pool", name: "Woolypooly" },
+  ],
+  [
+    "1FNapoV7CnCzmQWFFuSezhgjKGmz6897yGWafsRhWSV1k",
+    { type: "Pool", name: "Woolypooly" },
+  ],
+  [
+    "134RbnQXBWFCnR4HhmEgFjkotkdmNzNeYqhQ84ZKjHnjN",
+    { type: "Pool", name: "Woolypooly" },
+  ],
+  [
+    "1DKWCEc9HjhchKoMCMA4J1nbNGNjrAoPFnkcgA1w6h7jh",
+    { type: "Pool", name: "Woolypooly" },
+  ],
+  [
+    "12WhBVry3PXzTyCm389eSZMVsDSQZjGh5csv6CPrNfgtz",
+    { type: "Pool", name: "Woolypooly" },
+  ],
+  [
+    "1BU6eg4bnn4gFgpSx6U96xqwZxyJHwEdMajCE3wminc3e",
+    { type: "Pool", name: "Woolypooly" },
+  ],
+  [
+    "134dmgh9AZNimp9ZAYpWBTwv3ddouETBTyq6134mBvVk2",
+    { type: "Pool", name: "Woolypooly" },
+  ],
+  [
+    "1GY1EBQAW3tqHpwnRyon7M32VfU3k9K2EARdHPnosKjLW",
+    { type: "Pool", name: "Coinhunters" },
+  ],
+  [
+    "1BRuUfJeuaVAAh9QQRsP4DH7RdJL83TwHWNyWLgx69jXc",
+    { type: "Pool", name: "Coinhunters" },
+  ],
+  [
+    "1CaEXVpPVGHjA4BV8znXAYKM9jR11B28XZ9QnsByhbJBJ",
+    { type: "Pool", name: "Coinhunters" },
+  ],
+  [
+    "1EiSSuHFdJhDZgjZ5M6kHSoxDC1jrHBmhnLrz37Qc2DFU",
+    { type: "Pool", name: "Coinhunters" },
+  ],
+  [
+    "1EW4DKjCvXWEbipBNFxf1N3UDhdYmqX7NBmjjizvd9j74",
+    { type: "Pool", name: "Alph-pool" },
+  ],
+  [
+    "17yg3UYNodTsg6TjbymroUpiyp1aDXUJF2MA2AVbL85JT",
+    { type: "Pool", name: "Alph-pool" },
+  ],
+  [
+    "1CKDMPhxEgjCYUUoAtuq5q5CqeH3XQvj3MXjiARriWfRU",
+    { type: "Pool", name: "Alph-pool" },
+  ],
+  [
+    "13XkffE3aSgkKJLzE8xaE9JqhrRpfHkNmbSfzZaYqKMTW",
+    { type: "Pool", name: "Alph-pool" },
+  ],
+  [
+    "1Gzp3Wc42fanrbAqknYmSaZLfC567bP3JWprDCiuJ8rbP",
+    { type: "Pool", name: "Herominers" },
+  ],
+  [
+    "19WzSnmNC1SQ6v7RpFFXhpcMcFSiwM4nKTSdbwgSJfSHy",
+    { type: "Pool", name: "Herominers" },
+  ],
+  [
+    "14kSoi8pFMn2wMJFZJgGpFSA7GyNMtwWKnsKc1KidZ8Bu",
+    { type: "Pool", name: "Herominers" },
+  ],
+  [
+    "12jK2jHyyJTJyuRMRya7QJSojgVnb5yh4HVzNNw6BTBDF",
+    { type: "Pool", name: "Herominers" },
+  ],
+  [
+    "19TJU5W2YzwhLwtUXECA8AoFjxdk6CvVuLkaDczLWVCy6",
+    { type: "Pool", name: "Alephium-pool" },
+  ],
+  [
+    "126rZSbRyRM4xMUN3VUoVBgqiChMCopySRDHkmgtr6HWe",
+    { type: "Pool", name: "Alephium-pool" },
+  ],
+  [
+    "14vYkbiJZCcZ9ZfKuf7exu4XsjJH96CSJo7GPaq1wqJog",
+    { type: "Pool", name: "Alephium-pool" },
+  ],
+  [
+    "1GNZSTNTcJL3VcHj7izRHcn1xGuKAogcxhyPiLYADQY47",
+    { type: "Pool", name: "Alephium-pool" },
+  ],
+  [
+    "1CXiWiTroEdLJ4vYH8Sg6TVyCkgLPp33X7hqTiDcevYgD",
+    { type: "Pool", name: "Alephium-pool" },
+  ],
+  [
+    "189WnFopHTD15smsnZyvdVhF61pvmZaV52gtp1poiRmwg",
+    { type: "Pool", name: "Solopool" },
+  ],
+  [
+    "1F8BAnQ8TspUcpLKHzxyaUMZ8fw3YvT3NJ3VQuY43dnFA",
+    { type: "Pool", name: "Solopool" },
+  ],
 ]);
